c50154eb04a76f578230c794a2305046ff2e7463
TrongBinhLe <ltbinhbk2010@gmail.com>
2022-12-07 13:28:49 +0700

    Adding swipe action for UITableViewCell

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -53,6 +53,69 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    // Trailing Swipe
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // delete
+        let delete = UIContextualAction(style: .normal, title: "Delete") { action, view, completionHandler in
+            print("Delete: \(indexPath.row + 1)")
+            completionHandler(true)
+        }
+        delete.image = UIImage(systemName: "trash")
+        delete.backgroundColor = .red
+        
+        //share
+        let share = UIContextualAction(style: .normal, title: "Share", handler: { action, view, completionHandler in
+            print("Share: \(indexPath.row + 1) ")
+            completionHandler(true)
+        })
+        share.image = UIImage(systemName: "square.and.arrow.up")
+        share.backgroundColor = .blue
+        
+        //download
+        let download = UIContextualAction(style: .normal, title: "Download") { action, view, completionHandler in
+            print("Download: \(indexPath.row + 1)")
+            completionHandler(true)
+        }
+        download.image = UIImage(systemName: "arrow.down")
+        download.backgroundColor = .green
+        
+        let swipe = UISwipeActionsConfiguration(actions: [delete, share, download])
+        
+        return swipe
+    }
+    
+    //Leading Swipe
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        //favorite
+        let favorite = UIContextualAction(style: .normal, title: "favorite") { action, view, completionHandler in
+            print("Favorite: \(indexPath.row + 1)")
+            completionHandler(true)
+        }
+        favorite.image = UIImage(systemName: "suit.heart.fill")
+        favorite.backgroundColor = .systemPink
+        
+        //profile
+        let profile = UIContextualAction(style: .normal, title: "profile") { action, view, completionHandler in
+            print("Profile: \(indexPath.row + 1)")
+            completionHandler(true)
+        }
+        profile.image = UIImage(systemName: "person.fill")
+        profile.backgroundColor = .yellow
+    
+        //report
+        let report = UIContextualAction(style: .normal, title: "report") { action, view, completionHandler in
+            print("Report: \(indexPath.row + 1)")
+            completionHandler(true)
+        }
+        
+        report.image = UIImage(systemName: "person.crop.circle.badge.xmark")
+        report.backgroundColor = .lightGray
+        
+        let swipe = UISwipeActionsConfiguration(actions: [favorite, profile, report])
+        
+        return swipe
+    }
+    
     // MARK: Add New Items
     
     
